8b23f07d9c5df8fb8f79e3a8a9915bc0d4b52d4a
windwail <windwail@mail.ru>
2016-12-21 13:23:06 +0300

    .arrrh

--- webapp/src/app/app.module.js
+++ webapp/src/app/app.module.js
@@ -1,14 +1,30 @@
 (function() {
 
-	angular.module("uzedoApp", [])
+	angular.module("uzedoApp", ['ngMockE2E'])
 		.controller('MyCoolController',function($scope, $http) {
 
 			$scope.data = "Me";
+			$scope.cata = {};
 
 			$http.get('/customers?some=shit').then(function(response) {
 				console.log(response.data);
 				$scope.data = response.data;
 			});
+
+            $http.get('/data?some=shit').then(function(response) {
+                console.log(response.data);
+                $scope.cata = response.data;
+            });
+
+		})
+		.run(function($httpBackend){
+			console.log("httpBackend inited => "+!!$httpBackend)
+
+            $httpBackend
+                .whenGET(/^\/customers.*/)
+                .respond(["1", "2", "3"]);
+
+            $httpBackend.whenGET(/[\s\S]*/).passThrough();
 
 		});
 
