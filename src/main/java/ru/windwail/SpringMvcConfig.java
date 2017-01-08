b08c6d63d3b5cbecf0511041ddf44f5f932e2123
windwail@mail.ru <Valenok128>
2017-01-08 13:30:00 +0300

    Reconfigured for h2 database in file.

--- src/main/java/ru/windwail/SpringMvcConfig.java
+++ src/main/java/ru/windwail/SpringMvcConfig.java
@@ -1,5 +1,8 @@
 package ru.windwail;
 
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -21,5 +24,12 @@ public class SpringMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("redirect:/index.html");
+    }
+
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
     }
 }
